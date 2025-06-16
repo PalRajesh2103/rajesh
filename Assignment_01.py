55214716e94f733b760a2c67344b51403e3e5856
PalRajesh2103 <pal.rajesh2103@gmail.com>
2025-06-16 16:02:34 +0530

    Add files via upload

---
+++ Assignment_01.py
@@ -0,0 +1,32 @@
+'''
+# WAP to take input two numbers from user and perform the mathematical operation
+
+First_Number = float(input("First Number: "))
+Second_Number = float(input("Second Number: "))
+
+addition = First_Number+Second_Number
+print("Addition : ", addition)
+
+substraction = First_Number-Second_Number
+print("Substraction : ", substraction)
+
+multiplication = First_Number * Second_Number
+print("Multiplication : ", multiplication)
+
+division = First_Number / Second_Number
+print("Division : ", division)
+'''
+
+#WAP to python program  :
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+FirstName = input("FirstName: ")
+SecondName = input("SecondName: ")
+
+FullName = FirstName+SecondName+"!"
+
+print("Hello,",FullName,"Welcome to the python program")
+
+
